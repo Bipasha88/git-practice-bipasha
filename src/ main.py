3719d00b86085366817e2d8fbc5b979c044bc5f8
Bipasha88 <dolasaha88@gmail.com>
2026-02-27 12:57:08 +0600

    Add multiply function

--- src/ main.py
+++ src/ main.py
@@ -3,7 +3,8 @@ print("Bipasha Saha")
 from datetime import date
 print(date.today())
 
-from utils import add,subtract
+from utils import add,subtract,multiply
 
 print(add(10,5))
 print(subtract(10,5))
+print(multiply(10, 5))
